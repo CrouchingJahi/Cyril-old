41e88b2d3d8e5e4c771e945a1dbad2c51aca5fc8
Jahi Crouch <crouching.jahi@gmail.com>
2016-12-31 13:49:21 -0500

    Testing improvements

--- test/general.spec.js
+++ test/general.spec.js
@@ -1,15 +1,13 @@
-// import React from 'react';
-// import Home from '../app/ui/cyril.js';
-// import renderer from 'react-test-renderer';
-var React = require('react'),
-  Home = require('../app/ui/cyril.js').home;
+import React from 'react';
+import renderer from 'react-test-renderer';
+// var React = require('react'),
+  // Home = require('../app/ui/cyril.js').home;
 
-test('has a Home component', () => {
-  // const sum = require('../src/jsx/app.jsx');
-  // expect(sum(1, 2)).toBe(3);
+xdescribe('The root component', () => {
   // const component = renderer.create(<Home />);
-  const component = React.createElement(Home, null);
-  console.log(component);
-  expect(1+2).toBe(3);
-  // console.log(Home.render());
+  it('adds a listener on DOMContentLoaded', function () {
+    var component = require('../app/ui/cyril.jsx');
+    expect(1+2).toBe(3);
+    // expect();
+  });
 });
